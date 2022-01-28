07a4821b697b1b20f194eeec7f59ca9c727e6dbc
MarlyKhoury <marlykh625@gmail.com>
2022-01-28 18:43:47 +0200

    debugged

--- soundgame.js
+++ soundgame.js
@@ -7,15 +7,15 @@ var first_run=0
 var pc_sequence = []
 var user_input=0;
 var counter=0
-
+var count=1
 var round=0;
 
 
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keydown", function(event) {
     if(first_run==0){
 	generate_random() 
-    
+    document.querySelector("#title").innerHTML= "Level 1"
     }
 });
 
@@ -23,15 +23,15 @@ document.getElementById("red").addEventListener("click",function() {
     var beat = new Audio('sounds/red.mp3')
     beat.play()
     muted="muted"
-  user_input=0
-  check_true(user_input,pc_sequence)
+    user_input=0
+    check_true(user_input,pc_sequence)
     
   }) 
 
 document.getElementById("yellow").addEventListener("click",function() {
     var beat = new Audio('sounds/yellow.mp3')
-     beat.play()
-     muted="muted"
+    beat.play()
+    muted="muted"
     user_input=1
     check_true(user_input,pc_sequence) 
       
@@ -41,10 +41,11 @@ document.getElementById("green").addEventListener("click",function() {
     var beat = new Audio('sounds/green.mp3')
     beat.play()
     muted="muted"
-     user_input=2
+    user_input=2
     check_true(user_input,pc_sequence)
           
  }) 
+
 document.getElementById("blue").addEventListener("click",function() {
     var beat = new Audio('sounds/blue.mp3')
     beat.play()
@@ -52,7 +53,7 @@ document.getElementById("blue").addEventListener("click",function() {
     user_input=3
     check_true(user_input,pc_sequence)
               
-            }) 
+ }) 
 
 
 
@@ -60,15 +61,18 @@ function generate_random() {
     
     random_sequence = Math.floor(Math.random() * 4);
     pc_sequence.push(color_list[random_sequence])
-   var color_choice=color_list[random_sequence]
+    var color_choice=color_list[random_sequence]
     var beat = new Audio('sounds/'+color_choice+'.mp3')
     setTimeout(function(){
         beat.play()
     },1000)
-   muted="muted"
+    muted="muted"
    
    first_run=1
    round=pc_sequence.length  
+   count+=1
+   console.log(pc_sequence)
+   console.log(count)
 
 } 
 
@@ -77,9 +81,7 @@ function check_true(user_input,pc_sequence){
 if (counter<round) {
 
     if(color_list[user_input]==pc_sequence[counter]) {
-        console.log("correct")
-        console.log("user int input: "+user_input)
-        console.log("user color choice: "+color_list[user_input])
+        document.querySelector("#title").innerHTML= "Level "+ count
         counter++
     }
     else {
@@ -87,9 +89,11 @@ if (counter<round) {
       var beat = new Audio("sounds/wrong.mp3")
       beat.play()
       muted="muted"
-    
-    }
+  
+       }
+
 }
+
 
 
 if(counter==round) {
