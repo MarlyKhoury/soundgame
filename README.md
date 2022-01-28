fa03ada7bf31d583d504dd044773738f31273d7f
MarlyKhoury <marlykh625@gmail.com>
2022-01-28 20:16:21 +0200

    fixed conditions and added button event

--- README.md
+++ README.md
@@ -9,22 +9,10 @@
 - 🥅 2022 Goals: Graduate from boot-camp
 - ⚡ Fun fact: I love to travel and hike 
 
-### Connect with me:
 
-[<img align="left" alt="soundgame | LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" />][linkedin]
-[<img align="left" alt="soundgame | Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />][instagram]
-
-<br />
-
-### Languages and Tools:
+### GAME EXPLANATION:
 
 <img align="left" alt="First state of the game" width="300px" src="Screenshots/Screenshot (10).png">
 
 
-<br />
-<br />
 
----
-
-[instagram]: https://instagram.com/marlykhoury
-[linkedin]:  https://www.linkedin.com/in/marly-khoury-542501167/
--- soundgame.js
+++ soundgame.js
@@ -1,110 +1,147 @@
 
-
-var color_list = ["red","yellow","green","blue"]
+var color_list = ["green","red","yellow","blue"]
 var random_sequence = Math.floor(Math.random() * 4);
 
 var first_run=0
 var pc_sequence = []
 var user_input=0;
 var counter=0
-var count=1
+
 var round=0;
 
 
 
 document.addEventListener("keydown", function(event) {
-    if(first_run==0){
-	generate_random() 
-    document.querySelector("#title").innerHTML= "Level 1"
+  if(first_run==-1){
+    restart_game()
+    }
+    else if(first_run==0){
+        generate_random()  
+     
     }
 });
 
 document.getElementById("red").addEventListener("click",function() {
+  if(first_run!=-1){
+    document.getElementsByClassName("btn")[1].classList.add("pressed");
     var beat = new Audio('sounds/red.mp3')
     beat.play()
     muted="muted"
-    user_input=0
-    check_true(user_input,pc_sequence)
+  user_input=0
+  check_true(user_input,pc_sequence)
+  setTimeout(function(){
+    document.getElementsByClassName("btn")[1].classList.remove("pressed");
+  },100)
+
+  }
     
   }) 
 
 document.getElementById("yellow").addEventListener("click",function() {
+  if(first_run!=-1){
+    
+    document.getElementsByClassName("btn")[2].classList.add("pressed");
     var beat = new Audio('sounds/yellow.mp3')
-    beat.play()
-    muted="muted"
+     beat.play()
+     muted="muted"
     user_input=1
     check_true(user_input,pc_sequence) 
-      
+    setTimeout(function(){
+      document.getElementsByClassName("btn")[2].classList.remove("pressed");
+    },100)
+
+  }
     }) 
+    
 
 document.getElementById("green").addEventListener("click",function() {
+  if(first_run!=-1){
+
+    document.getElementsByClassName("btn")[0].classList.add("pressed");
     var beat = new Audio('sounds/green.mp3')
     beat.play()
     muted="muted"
-    user_input=2
+     user_input=2
     check_true(user_input,pc_sequence)
-          
+    setTimeout(function(){
+      document.getElementsByClassName("btn")[0].classList.remove("pressed");
+    },100)
+  }    
  }) 
-
 document.getElementById("blue").addEventListener("click",function() {
+  if(first_run!=-1){
+
+    document.getElementsByClassName("btn")[3].classList.add("pressed");
     var beat = new Audio('sounds/blue.mp3')
     beat.play()
     muted="muted"
     user_input=3
     check_true(user_input,pc_sequence)
-              
+    setTimeout(function(){
+      document.getElementsByClassName("btn")[3].classList.remove("pressed");
+    },100)
+  }    
  }) 
 
 
-
 function generate_random() {   
-    
+
     random_sequence = Math.floor(Math.random() * 4);
     pc_sequence.push(color_list[random_sequence])
+    document.getElementsByClassName("btn")[random_sequence].classList.add("pressed");
+
+  
     var color_choice=color_list[random_sequence]
     var beat = new Audio('sounds/'+color_choice+'.mp3')
     setTimeout(function(){
-        beat.play()
+       beat.play()
     },1000)
-    muted="muted"
+   muted="muted"
    
    first_run=1
    round=pc_sequence.length  
-   count+=1
-   console.log(pc_sequence)
-   console.log(count)
+   document.getElementById("title").innerHTML="LeveL "+round
+   setTimeout(function(){
+
+  document.getElementsByClassName("btn")[random_sequence].classList.remove("pressed");
+  },100)
+
 
 } 
 
 
 function check_true(user_input,pc_sequence){
-if (counter<round) {
+    if (counter<round) {
 
-    if(color_list[user_input]==pc_sequence[counter]) {
-        document.querySelector("#title").innerHTML= "Level "+ count
-        counter++
+        if(color_list[user_input]==pc_sequence[counter]) {
+            console.log("correct")
+            counter++
+        }
+        else {
+          document.querySelector("#title").innerHTML= "Game Over, Press Any Key to Restart"
+          var beat = new Audio("sounds/wrong.mp3")
+        beat.play()
+          muted="muted"
+          console.log("wrong")
+          first_run=-1       
+        }
     }
-    else {
-      document.querySelector("#title").innerHTML= "Game Over, Press Any Key to Restart"
-      setTimeout(function(){document.querySelector("body").style.background= "red"},1000)
-      var beat = new Audio("sounds/wrong.mp3")
-      beat.play()
-      muted="muted"
-  
-       }
 
+    if(counter==round) {
+      setTimeout(function(){
+        generate_random()
+        counter=0
+      },1000)
+    }
 }
 
+function restart_game(){
 
+  first_run=0
+  pc_sequence = []
+  user_input=0;
+  counter=0
+  round=0;
 
-if(counter==round) {
-    generate_random()
-    counter=0
 }
-
-}
-    
-      
-
-
 
